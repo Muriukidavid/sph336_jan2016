0dc05ca99f12f31980697303584cb5680ad486b3
Joshua Nyantika Mokua <joshuanyantika@students.uonbi.ac.ke>
2016-02-25 13:51:51 +0300

    Add an extra line of output.

--- test.cc
+++ test.cc
@@ -4,6 +4,7 @@ using namespace std;
 int main(int argc, char *argv[]){
 
 cout<<"hello world"<<endl;
+cout<<"This is my first program"<<endl;
 cout<<argc<<endl<<argv[0]<<endl<<argv[1]<<endl;
 return 0;
 }
