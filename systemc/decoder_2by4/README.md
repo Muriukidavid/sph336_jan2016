e6ce9f343c963c2ef39e82543345dce617d99516
Agatha22 <doreenleonards@gmail.com>
2016-03-31 09:02:08 +0300

    A 2BY4_DECODER

---
+++ systemc/decoder_2by4/README.md
@@ -0,0 +1,21 @@
+Name : Mwai Moses Muraguri 
+reg no :i39/4458/2014
+ 
+A 2 by 4 decoder takes two inputs and produces 4 outputs.
+It is normally made by combining 2 1 by 2 decoders.
+
+Its truth table: 
+<p align="left">
+  <img src="truth_table.png" width="250"/>
+</p>
+
+
+Circuit:
+<p align="left">
+  <img src="circuit.png" width="200"/>
+</p>
+
+Model of computation:
+<p align="left">
+  <img src="MOC.png" width="400"/>
+</p>
---
+++ systemc/decoder_2by4/dec2by4.h
@@ -0,0 +1,39 @@
+/*
+ * decoder_2by4.h
+ *
+ *  Created on: Mar 4, 2016
+ *      Author: karibe
+ */
+
+#ifndef DECODER_2BY4_H_
+#define DECODER_2BY4_H_
+#include<systemc.h>
+
+SC_MODULE(decoder){
+//input and output ports
+sc_in<bool> a1,a2;
+sc_out<bool> d1,d2,d3,d4;
+//constructor: where the processes are bound to simulation kernel
+SC_CTOR(decoder){
+	SC_METHOD(decode);
+	sensitive<<a1;
+	sensitive<<a2;
+	//dont_initialize();
+}
+
+~decoder(){
+//delete stuff :P
+}
+
+void decode(void){
+	d1=(a1==0 && a2==0)?1:0;
+	d2=(a1==0 && a2 ==1)?1:0;
+	d3=(a1==1  && a2 ==0)?1:0;
+	d4=(a1==1 && a2 ==1)?1:0;
+}
+};
+
+
+
+
+#endif /* DECODER_2BY4_H_ */
