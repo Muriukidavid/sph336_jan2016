a097e7707fac62e2176a3335207b42107603d524
OGENDI <demokonge17@gmail.com>
2016-07-19 08:00:32 +0300

    ADD CODE FOR QUEUE IMPLEMENTATION
    Include 3 files
    Main.c , Derick.c and Derick.h
    They simpley execute a simple queue program.

---
+++ Derick/queue/main.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include "Derick.h"
+        const int MAX = 5;
+          struct que q;
+
+
+int main(){
+	 q.front =-1;
+         q.rear=-1;
+	int choice;
+	char num;
+	while(1){
+		printf("Enter your choice :\n ");
+		printf("\n1.Add\n");
+		printf("2.Remove\n");
+		printf("3.Display\n");
+		printf("4.Exit\n");
+		scanf("%d",&choice);
+		switch(choice){
+			case 1:
+						printf("\nEnter a character to be inserted : ");
+						scanf(" %c",&num);
+						insert(num);
+						break;
+			case 2:
+						del();
+
+
+						break;
+			case 3:
+						display();
+						break;
+			case 4:
+						exit(-1);
+
+			default:
+						printf("Invalid choice\n");
+
+
+
+		}//end of switch
+
+
+	}//end of while loop
+}//end of main function
